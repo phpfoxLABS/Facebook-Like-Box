a121f6d2c95870949edb5fbe691fc19b7e8ef26b
Jacob Adams <jacob@phpfoxlabs.com>
2011-05-06 22:02:06 -0400

    added module

---
+++ upload/module/fbplugin/include/component/block/likebox.class.php
@@ -0,0 +1,56 @@
+<?php
+
+ /**
+ * 
+ * @copyright		phpfoxLABS.com
+ * @author  		Jacob Adams
+ * @package  		Facebook Social Plugins
+ * @version 		v1.0.0 - 2011-04-5
+ * 
+ *
+ */
+
+
+defined('PHPFOX') or exit('NO DICE!');
+
+
+class Fbplugin_Component_Block_likebox extends Phpfox_Component
+{
+	
+	public function process()
+	{
+		
+		
+		
+		if (!Phpfox::getParam('fbplugin.facebook_page_url'))
+		{
+			return false;
+	
+	
+		}
+	
+	
+
+	
+		$this->template()->assign(array(
+										'sHeader' => Phpfox::getPhrase('fbplugin.like_box_title'),
+										'sPageURL' => urlencode(Phpfox::getParam('fbplugin.facebook_page_url')),
+										'sPluginBG' => Phpfox::getParam('fbplugin.plugin_bg_color'),
+										'sPluginScheme' => Phpfox::getParam('fbplugin.color_scheme'),
+										'sPluginWidth' => Phpfox::getParam('fbplugin.side_width'),
+										'sPluginCSSWidth' => (Phpfox::getParam('fbplugin.side_width') - 3)
+										));
+	
+		
+		
+			return 'block';	
+ 
+ 
+ 
+	}
+	
+	
+}
+
+
+?>
---
+++ upload/module/fbplugin/template/default/block/likebox.html.php
@@ -0,0 +1,41 @@
+<?php
+
+ /**
+ * 
+ * @copyright		phpfoxLABS.com
+ * @author  		Jacob Adams
+ * @package  		Facebook Social Plugins
+ * @version 		v1.0.0 - 2011-04-5
+ * 
+ *
+ */
+ 
+ 
+defined('PHPFOX') or exit('NO DICE!'); 
+
+?>
+
+
+{if isset($sPageURL)}
+{literal}
+
+<style type="text/css">
+<!--
+
+
+div.likeboxwrap {
+width:{/literal}{$sPluginCSSWidth}{literal}px;
+height:547px;
+overflow:hidden;
+}
+
+div.likeboxwrap iframe {margin:-1px;}
+
+
+-->
+</style>
+{/literal}
+<div class="likeboxwrap">
+<iframe src="http://www.facebook.com/plugins/likebox.php?href={$sPageURL}&amp;width={$sPluginWidth}&amp;border=no&amp;colorscheme={$sPluginScheme}&amp;show_faces=true&amp;stream=true&amp;header=false&amp;height=550" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:{$sPluginWidth}px; height:550px;  margin:-1px;" allowTransparency="true"></iframe>
+</div>
+{/if}
